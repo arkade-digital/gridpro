5f66c1f05aed7ca948f99996091f5f4d5ec300dc
Thai Phan <thai@outlook.com>
2016-05-25 20:10:49 +1000

    Add linting to SCSS
    Also updated the SCSS so that it passes linting.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var sassLint = require('gulp-sass-lint');
 
-gulp.task('default', function() {
+gulp.task('lint', () => {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+    ;
+});
+
+gulp.task('default', ['lint'], () => {
     return gulp.src('sass/gridpro.scss')
         .pipe(sass())
         .pipe(gulp.dest('dist/css'))
